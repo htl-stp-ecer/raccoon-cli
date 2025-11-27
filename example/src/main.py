fd6b509a316cf48664b14d0c3dd9bfca58c49941
Tobias Madlberger <tobias.madlberger@gmail.com>
2025-11-27 21:43:28 +0100

    refactor drive_to_potation_mission.py to enhance movement sequence; update motor calibration parameters in raccoon.project.yml

--- example/src/main.py
+++ example/src/main.py
@@ -1,8 +1,22 @@
-import time
+from libstp.calibration import CalibrationConfig
 
 from src.hardware.robot import Robot
 
 robot = Robot()
+
+# # Default calibration
+# results = robot.kinematics.calibrate_motors()
+# #
+# # # Custom config (works for both differential and mecanum)
+# # config = CalibrationConfig()
+# # config.use_relay_feedback = True  # Aggressive mode
+# # results = robot.drive.calibrate_motors(config)
+#
+# # Check results
+# for i, result in enumerate(results):
+#   if result.success:
+#       print(f"Motor {i}: PID: kp={result.pid.kp}, ki={result.pid.ki}, kd={result.pid.kd}, FF: kS={result.ff.kS}, kV={result.ff.kV}, kA={result.ff.kA}")
+#
 
 if __name__ == "__main__":
     robot.start()
--- example/src/missions/drive_to_potation_mission.py
+++ example/src/missions/drive_to_potation_mission.py
@@ -1,8 +1,11 @@
 from libstp.mission.api import Mission
 from libstp.robot.api import GenericRobot
 from libstp.step import Step
-from libstp.step.drive import drive_forward
+from libstp.step.wait_for_seconds import wait
+from libstp.step.drive import drive_forward, drive_backward
+from libstp.step.turn import turn_cw, turn_ccw
 from libstp.step.sequential import Sequential, seq
+from libstp.step.strafe import strafe_left
 
 
 
@@ -15,10 +18,11 @@ class G(Step):
 class DriveToPotatoMission(Mission):
     def sequence(self) -> Sequential:
         return seq([
-            G(),
-            drive_forward(cm=10),
-            # timeout(2),
-            # drive_backward(cm=5),
+            turn_cw(90, 1),
+            drive_forward(cm=25),
+            strafe_left(20, 1),
+            #wait(2),
+            turn_ccw(90, 1)
             # parallel(
             # ),
         ])
