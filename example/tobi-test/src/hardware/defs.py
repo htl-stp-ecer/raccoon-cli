02675083cd1f04cb4983ca2a8759029de565545c
Raccoon Auto History <raccoon@local>
2026-02-14 12:03:54 +0100

    chore(sync): pre-push snapshot
    Direction: push
    Target: pi@192.168.178.85:/home/pi/programs/21e5bb71-a37d-4a2f-99ed-ad10963fe875
    Summary: 4 files (+0 ~4 -0)
    
    Changed files:
    - M example/tobi-test/raccoon.project.yml
    - M example/tobi-test/src/hardware/.raccoon_codegen_cache.json
    - M example/tobi-test/src/hardware/defs.py
    - M example/tobi-test/src/hardware/robot.py

--- example/tobi-test/src/hardware/defs.py
+++ example/tobi-test/src/hardware/defs.py
@@ -1,7 +1,7 @@
 """
 ===========================================================
  Project:   tobi-test
- Generated: 2026-02-14 10:03:16
+ Generated: 2026-02-14 11:03:14
 ===========================================================
 
 Authors:
@@ -32,7 +32,7 @@ class Defs:
         calibration=MotorCalibration(
             ff=Feedforward(kS=0.08, kV=0.12, kA=0.1),
             pid=PidGains(kp=2.4, ki=0.3, kd=0.08),
-            ticks_to_rad=2.0044420934875653e-05,
+            ticks_to_rad=2.0957505928082034e-05,
             vel_lpf_alpha=0.8,
         ),
     )
@@ -42,7 +42,7 @@ class Defs:
         calibration=MotorCalibration(
             ff=Feedforward(kS=0.08, kV=0.12, kA=0.1),
             pid=PidGains(kp=2.4, ki=0.3, kd=0.08),
-            ticks_to_rad=1.6724483734774104e-05,
+            ticks_to_rad=1.6597344394675888e-05,
             vel_lpf_alpha=0.8,
         ),
     )
@@ -52,7 +52,7 @@ class Defs:
         calibration=MotorCalibration(
             ff=Feedforward(kS=0.08, kV=0.12, kA=0.1),
             pid=PidGains(kp=2.4, ki=0.3, kd=0.08),
-            ticks_to_rad=1.8790607135078375e-05,
+            ticks_to_rad=1.849598834088757e-05,
             vel_lpf_alpha=0.8,
         ),
     )
@@ -62,7 +62,7 @@ class Defs:
         calibration=MotorCalibration(
             ff=Feedforward(kS=0.08, kV=0.12, kA=0.1),
             pid=PidGains(kp=2.4, ki=0.3, kd=0.08),
-            ticks_to_rad=1.9275086425259718e-05,
+            ticks_to_rad=1.98506995308817e-05,
             vel_lpf_alpha=0.8,
         ),
     )
