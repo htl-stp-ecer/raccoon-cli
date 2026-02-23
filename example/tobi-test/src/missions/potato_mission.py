34e612c47a9031cd611658ead11ad3403ce6c07a
Raccoon Auto History <raccoon@local>
2026-02-23 08:38:12 +0100

    chore(sync): pre-push snapshot
    Direction: push
    Target: pi@10.70.146.134:/home/pi/programs/21e5bb71-a37d-4a2f-99ed-ad10963fe875
    Summary: 2 files (+0 ~2 -0)
    
    Changed files:
    - M example/tobi-test/raccoon.project.yml
    - M example/tobi-test/src/missions/potato_mission.py

--- example/tobi-test/src/missions/potato_mission.py
+++ example/tobi-test/src/missions/potato_mission.py
@@ -1,4 +1,4 @@
-from libstp import drive_forward, auto_tune
+from libstp import drive_forward, auto_tune, turn_right
 from libstp.mission.api import Mission
 from libstp.step.sequential import Sequential, seq
 
@@ -7,8 +7,8 @@ class PotatoMission(Mission):
     def sequence(self) -> Sequential:
         return seq([
             # turn_left(90),
-            # turn_right(90),
-            drive_forward(cm=50)
+            turn_right(90),
+            # drive_forward(cm=50)
             # auto_tune(),
             # tune_drive(),
             # characterize_drive(
