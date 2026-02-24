1c7c42b241cb538abe2678973cf74703730822bd
Raccoon Auto History <raccoon@local>
2026-02-24 17:27:00 +0100

    chore(sync): pre-push snapshot
    Direction: push
    Target: pi@192.168.100.81:/home/pi/programs/21e5bb71-a37d-4a2f-99ed-ad10963fe875
    Summary: 4 files (+0 ~4 -0)
    
    Changed files:
    - M example/tobi-test/raccoon.project.yml
    - M example/tobi-test/src/main.py
    - M example/tobi-test/src/missions/potato_mission.py
    - M example/tobi-test/src/missions/setup_mission.py

--- example/tobi-test/src/missions/potato_mission.py
+++ example/tobi-test/src/missions/potato_mission.py
@@ -1,4 +1,4 @@
-from libstp import turn_left, auto_tune, drive_forward, turn_right, drive_backward, follow_line
+from libstp import turn_left, auto_tune, drive_forward, turn_right, drive_backward
 from libstp.mission.api import Mission
 from libstp.step.sequential import Sequential, seq
 
@@ -6,10 +6,9 @@ from libstp.step.sequential import Sequential, seq
 class PotatoMission(Mission):
     def sequence(self) -> Sequential:
         return seq([
-            auto_tune(),
-            #turn_left(90),
-            follow_line(cm=50)
-            #turn_right(90),
+            #auto_tune()
+            turn_left(90),
+            turn_right(90),
             #drive_forward(cm=50),
             #drive_backward(cm=50)
             # auto_tune(),
