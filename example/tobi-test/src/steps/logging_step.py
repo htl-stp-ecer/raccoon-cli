da34ceb485fa5e9217ef780ea80c037712577a14
Raccoon Auto History <raccoon@local>
2026-02-26 20:18:22 +0100

    chore(sync): pre-push snapshot
    Direction: push
    Target: pi@192.168.178.85:/home/pi/programs/21e5bb71-a37d-4a2f-99ed-ad10963fe875
    Summary: 2 files (+0 ~2 -0)
    
    Changed files:
    - M example/tobi-test/raccoon.project.yml
    - M example/tobi-test/src/steps/logging_step.py

--- example/tobi-test/src/steps/logging_step.py
+++ example/tobi-test/src/steps/logging_step.py
@@ -4,11 +4,11 @@ import libstp.foundation as logging
 
 class LoggingStep(Step):
     async def _execute_step(self, robot: "GenericRobot") -> None:
-        logging.set_global_level(logging.Level.trace)
-        logging.set_file_level("LcmReader.cpp", logging.Level.info)
+        #logging.set_global_level(logging.Level.trace)
+        #logging.set_file_level("LcmReader.cpp", logging.Level.info)
         # logging.set_file_level("turn_motion.cpp", logging.Level.trace)
         # logging.set_file_level("fused_odometry.cpp", logging.Level.info)
         # logging.set_file_level("drive.cpp", logging.Level.trace)
         # logging.set_file_level("motor_adapter.cpp", logging.Level.trace)
-        # logging.set_file_level("line_follow.py", logging.Level.trace)
-        # logging.set_file_level("base.py", logging.Level.trace)
+        logging.set_file_level("line_follow.py", logging.Level.trace)
+        logging.set_file_level("base.py", logging.Level.trace)
