5e61dc61dacb521cdc8dcebad43b99257836a096
Raccoon Auto History <raccoon@local>
2026-02-26 18:11:13 +0100

    chore(sync): pre-push snapshot
    Direction: push
    Target: pi@192.168.178.85:/home/pi/programs/21e5bb71-a37d-4a2f-99ed-ad10963fe875
    Summary: 1 files (+0 ~1 -0)
    
    Changed files:
    - M example/tobi-test/src/main.py

--- example/tobi-test/src/main.py
+++ example/tobi-test/src/main.py
@@ -26,7 +26,7 @@ robot = Robot()
 import libstp.foundation as logging
 
 logging.set_global_level(logging.Level.trace)
-#logging.set_file_level("LcmReader.cpp", logging.Level.info)
+logging.set_file_level("LcmReader.cpp", logging.Level.debug)
 #logging.set_file_level("turn_motion.cpp", logging.Level.trace)
 #logging.set_file_level("fused_odometry.cpp", logging.Level.info)
 #logging.set_file_level("drive.cpp", logging.Level.trace)
