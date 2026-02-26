8b28c7d249b5bc4d0b27db82663220b64bccb258
Raccoon Auto History <raccoon@local>
2026-02-26 17:41:38 +0100

    chore(sync): pre-push snapshot
    Direction: push
    Target: pi@192.168.178.85:/home/pi/programs/21e5bb71-a37d-4a2f-99ed-ad10963fe875
    Summary: 3 files (+0 ~3 -0)
    
    Changed files:
    - M example/tobi-test/src/hardware/.raccoon_codegen_cache.json
    - M example/tobi-test/src/hardware/defs.py
    - M example/tobi-test/src/hardware/robot.py

--- example/tobi-test/src/hardware/defs.py
+++ example/tobi-test/src/hardware/defs.py
@@ -1,7 +1,7 @@
 """
 ===========================================================
  Project:   tobi-test
- Generated: 2026-02-26 16:29:40
+ Generated: 2026-02-26 16:34:40
 ===========================================================
 
 Authors:
@@ -27,14 +27,14 @@ class Defs:
         port=0,
         inverted=False,
         calibration=MotorCalibration(
-            ticks_to_rad=1.7335059924678334e-05, vel_lpf_alpha=1.0
+            ticks_to_rad=1.730693183669234e-05, vel_lpf_alpha=1.0
         ),
     )
     front_right_motor = Motor(
         port=1,
         inverted=False,
         calibration=MotorCalibration(
-            ticks_to_rad=1.5089888622373892e-05, vel_lpf_alpha=1.0
+            ticks_to_rad=1.5227406920246178e-05, vel_lpf_alpha=1.0
         ),
     )
     front_left_ir_sensor = IRSensor(port=0)
