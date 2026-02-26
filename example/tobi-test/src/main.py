0814beda684536af2384ccd9c00c50267b9a7de1
Raccoon Auto History <raccoon@local>
2026-02-26 18:45:54 +0100

    chore(sync): pre-push snapshot
    Direction: push
    Target: pi@192.168.178.85:/home/pi/programs/21e5bb71-a37d-4a2f-99ed-ad10963fe875
    Summary: 2 files (+0 ~2 -0)
    
    Changed files:
    - M example/tobi-test/raccoon.project.yml
    - M example/tobi-test/src/main.py

--- example/tobi-test/src/main.py
+++ example/tobi-test/src/main.py
@@ -31,7 +31,8 @@ import libstp.foundation as logging
 #logging.set_file_level("fused_odometry.cpp", logging.Level.info)
 #logging.set_file_level("drive.cpp", logging.Level.trace)
 #logging.set_file_level("motor_adapter.cpp", logging.Level.trace)
-#logging.set_file_level("line_follower.py", logging.Level.trace)
+logging.set_file_level("line_follow.py", logging.Level.trace)
+logging.set_file_level("base.py", logging.Level.trace)
 
 if __name__ == "__main__":
     #set_digital(10)
